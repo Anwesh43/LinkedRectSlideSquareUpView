bbe529c04276ef09221ed9504b4515052779bcd9
Anwesh43 <anweshthecool0@gmail.com>
2021-02-06 02:44:31 +0530

    adding Canvas extension methods

--- rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
+++ rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
@@ -29,3 +29,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRectSlideSquareUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val squareSize : Float = Math.min(w, h) / squareSizeFactor
+    val rectSize : Float = h / rectSizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2, h / 2 * (1f - 2 * sf1))
+        drawRect(RectF(0f, 0f, squareSize, squareSize), paint)
+        restore()
+    }
+    save()
+    translate(-w / 2, h / 2)
+    drawRect(RectF(0f, -rectSize, w * sf2, 0f), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawRSSUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRectSlideSquareUp(scale, w, h, paint)
+}
+
