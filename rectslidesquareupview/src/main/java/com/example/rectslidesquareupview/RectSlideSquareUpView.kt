4360d5b2f4664c0a759346d96bd8881217864eea
Anwesh43 <anweshthecool0@gmail.com>
2021-02-06 02:45:43 +0530

    adding RectSlideSquareUpView class

--- rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
+++ rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
@@ -59,3 +59,18 @@ fun Canvas.drawRSSUNode(i : Int, scale : Float, paint : Paint) {
     drawRectSlideSquareUp(scale, w, h, paint)
 }
 
+class RectSlideSquareUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
