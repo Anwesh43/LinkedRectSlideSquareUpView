8564ea034b39811d57d3d327355a5b7d2eb1b750
Anwesh43 <anweshthecool0@gmail.com>
2021-02-06 03:23:49 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedrectslidesquareupview/MainActivity.kt
+++ app/src/main/java/com/example/linkedrectslidesquareupview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedrectslidesquareupview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.rectslidesquareupview.RectSlideSquareUpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectSlideSquareUpView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
