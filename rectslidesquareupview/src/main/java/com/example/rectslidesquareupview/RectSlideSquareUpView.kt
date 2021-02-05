8d771a0b99adbec45de2bcd7dfbdfe56a971d395
Anwesh43 <anweshthecool0@gmail.com>
2021-02-06 02:30:52 +0530

    adding constants and libraries

--- rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
+++ rectslidesquareupview/src/main/java/com/example/rectslidesquareupview/RectSlideSquareUpView.kt
@@ -1,2 +1,26 @@
 package com.example.rectslidesquareupview
 
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.view.View
+import android.view.MotionEvent
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#2196F3",
+    "#8BC34A",
+    "#FF5722",
+    "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val squareSizeFactor : Float = 5.9f
+val rectSizeFactor : Float = 6.9f
